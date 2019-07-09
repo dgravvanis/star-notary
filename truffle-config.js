660511eae2d8fbe17dfc921fff80f8a57634fc32
Dimitris Gravvanis <dgravvanis@icloud.com>
2019-07-09 14:28:31 +0300

    Update truffle-config.js
    Configure truffle-config.js to support deploying the contract to Rinkeby 
    Public Network. The mnemonic and the infura key, are stored in 
    uncommited files.

--- truffle-config.js
+++ truffle-config.js
@@ -22,11 +22,10 @@
  *
  */
 
-// const HDWallet = require('truffle-hdwallet-provider');
-// const infuraKey = "fj4jll3k.....";
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
+const infuraKey = fs.readFileSync(".infuraKey").toString().trim();
 
 module.exports = {
   /**
@@ -72,7 +71,11 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     // },
-
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,
+      production: true
+    }
     // Useful for private networks
     // private: {
       // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
